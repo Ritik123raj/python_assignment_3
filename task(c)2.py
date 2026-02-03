a5d8fababe364e37fe373a07d948760dab3834ea
Ritik123raj <92514871+Ritik123raj@users.noreply.github.com>
2026-02-03 11:26:11 +0530

    Add files via upload

---
+++ task(c)2.py
@@ -0,0 +1,7 @@
+import math
+
+a = int(input("Enter a Number: "))
+
+print(f"Square root of number {a} is {math.sqrt(a)}")
+print(f"Logarithm of number {a} is {math.log(a)}")
+print(f"Sine of number {a} is {math.sin(a)}")
